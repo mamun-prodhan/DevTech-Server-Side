7e4d78ebcfd601d45d477572b9bd0fd93cf178b9
mamun-prodhan <mamunpr9@gmail.com>
2022-10-31 00:08:40 +0600

    api created

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) =>{
     res.send('Course API Running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () =>{
     console.log('Course server running on port', port);
