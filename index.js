7325925876f5b116656e0844faec3b17d80e6732
mamun-prodhan <mamunpr9@gmail.com>
2022-10-31 16:25:45 +0600

    api updated and image added

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ app.get('/', (req, res) =>{
     res.send('Course API Running');
 });
 
+
+
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourses = courses.find( course => course.course_id === id);
+    res.send(selectedCourses);
+})
+
+// app.get('/details/:id', (req, res) =>{
+//     const id = req.params.id;
+//     const courseDetails = courses.find( course => course._id === id);
+//     res.send(courseDetails);
+// })
+
+
+
 
 app.listen(port, () =>{
     console.log('Course server running on port', port);
