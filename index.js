3a8e49369680edb0b3ce707bd3a113b1688afd91
mamun-prodhan <mamunpr9@gmail.com>
2022-11-03 12:42:00 +0600

    Final commit, project completed

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//cross origin resource sharing
 app.use(cors());
 
 const courses = require('./data/courses.json');
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Course API Running');
 });
 
@@ -15,17 +16,17 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
-app.get('/courses/:id', (req, res) =>{
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourses = courses.find( course => course.course_id === id);
+    const selectedCourses = courses.find(course => course.course_id === id);
     res.send(selectedCourses);
 })
 
-app.get('/details/:id', (req, res) =>{
+app.get('/details/:id', (req, res) => {
     const id = req.params.id;
-    const courseDetails = courses.find( course => course._id === id);
+    const courseDetails = courses.find(course => course._id === id);
     res.send(courseDetails);
 })
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Course server running on port', port);
 })
